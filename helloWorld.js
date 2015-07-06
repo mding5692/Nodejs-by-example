988ce112b82908e660c548c588c4c26f50317e16
Michael <m.ding5692@gmail.com>
2015-07-06 14:45:33 -0400

    Update and rename server.js to helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,5 @@
 var http = require("http");
-
+// program written as client-server program where Hello World is displayed on port 3000
 http.createServer(function(req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.write("Hello World!");
