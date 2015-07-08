5d5ddca5ba46e90a98e3d89daeeeedd327ae8576
Michael <m.ding5692@gmail.com>
2015-07-08 01:23:12 -0400

    Create modules.js

---
+++ modules.js
@@ -0,0 +1,13 @@
+// creating an example of a nodejs module
+
+  var helloWorld = function() {
+    console.log("hello World");
+    }
+  
+  var sayWhat = 1;
+
+/* module.exports starts as a js object */
+module.exports = {
+  func1 : helloWorld,
+  func2 : sayWhat
+  };
