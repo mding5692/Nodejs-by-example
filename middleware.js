1608f1e749abb31df028276df8e4dcd7ff8be022
Michael <m.ding5692@gmail.com>
2015-07-11 16:54:37 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -18,3 +18,14 @@ console.log('server running on port 3000');
 var app = connect()
 .listen(3000);
 console.log('server running on port 3000');
+
+// connect has a use method which takes three arguments: request, response and a next callback
+// next allows you to optionally pass control onto the next middleware registered with connect or inform connect about an error
+
+// Create a connect dispatcher and register with http
+var app = connect()
+// register a middleware
+.use(function (req, res, next) { next(); })
+.listen(3000);
+console.log('server running on port 3000');
+
