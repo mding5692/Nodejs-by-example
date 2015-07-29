e057886bae917678763edc0a3ea1984f780e1f44
Michael <m.ding5692@gmail.com>
2015-07-28 22:41:02 -0400

    Create introToDatabases.js

---
+++ introToDatabases.js
@@ -0,0 +1,4 @@
+// NoSQL (Not only SQL) databases will be discussed here, as they were made to meet problems with SQL databases
+
+// theres four broad categories: document-based (mongodb), key-valued (redis), column-family (cassandra) and graph databases (Neo4J)
+// these databases are all designed to be very scalable. Document-based provide most amount of features while maintaining good performance while key-value is simple and provides best performance for simple queries
