0bde356b807b1bfae4ce813936439d79be0140ae
Michael <m.ding5692@gmail.com>
2015-08-07 00:19:44 -0400

    Update introToPromises.js

--- introToPromises.js
+++ introToPromises.js
@@ -52,3 +52,72 @@ willReject
   // also, it is conventional to reject a promise with an Error object since it provides you with a stack-trace
   
   
+  //You can create an already fulfilled promise using the *when* member function
+var Q = require('q');
+Q.when(null).then(function (val) {
+  console.log(val == null); // true
+});
+Q.when('kung foo').then(function (val) {
+  console.log(val); // kung foo
+});
+console.log('I will print first because *then* is always async!');
+// callbacks and async code execute after sync code
+//Promise advantage: Promises do not suffer from the maybe sync problem of callbacks. If you want to return an
+//immediate promise, just return a resolved promise using Q.when and any then that the user registers is bound to be
+// called asynchronously.
+
+//there is a Q.reject function that creates an already rejected promise
+var Q = require('q');
+Q.reject(new Error('denied')).catch(function (err) {
+  console.log(err.message); // denied
+});
+
+/* The chainability of promises is one of its biggest features */
+// you can chain promises 
+var Q = require('q');
+Q.when(null)
+  .then(function () {
+    return 'kung foo';
+  })
+  .then(function (val) {
+    console.log(val); // kung foo
+    return Q.when('panda');
+  })
+  .then(function (val) {
+    console.log(val); // panda
+    // Nothing returned
+  })
+  .then(function (val) {
+    console.log(val == undefined); // true
+  });
+  // the then handlers will wait for first to return the promise before taking it in
+
+
+// if the promise is rejected, no further onFulfilled handlers are called
+// instead it waits until you hit a onRejected handler and the chain continues on with the value from the onRejected handler
+var Q = require('q');
+Q.when(null)
+  .then(function () {
+    throw new Error('panda'); // uncaught exception
+  })
+  .then(function (val) {
+    console.log('!!!!!', val); // I will never get called
+  })
+  .catch(function (reason) {
+    console.log('Someone threw a', reason.message);
+    return 'all good';
+  })
+  .then(function (val) {
+    console.log(val); // all good
+    return Q.reject(new Error('taco'));
+  })
+  .then(function (val) {
+    console.log('!!!!!', val); // I will never get called
+  })
+  .catch(function (reason) {
+    console.log('Someone threw a', reason.message);
+  });
+  
+  // Promise advantage: Uncaught exceptions inside onFulfilled/onRejected handlers do not blow the  
+  //application. Instead, they result in the promise in the chain to be rejected, which you can handle gracefully with a
+  //final onRejected handler.
